9699d4c3749fa4b32e478f58daf94aef03db01ef
vincent51689453 <vincent51689453@gmail.com>
2021-06-03 11:23:29 +0800

    fixed cannot display issue

--- start_camera_detect.sh
+++ start_camera_detect.sh
@@ -1,2 +1,3 @@
 cd standard
+export DISPLAY=:1
 python3 detect.py --source 0 --conf 0.5
--- start_image_detect.sh
+++ start_image_detect.sh
@@ -1,2 +1,3 @@
 cd standard
+export DISPLAY=:1
 python3 standard/detect.py --source ./data/images/zidane.jpg --conf 0.5
